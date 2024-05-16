0dded748121b3c1040aba52e8cf50e690d794eb5
xxxspy <xxxspy@126.com>
2024-05-16 15:07:06 +0800

    save

---
+++ mpluspy/mplus.py
@@ -0,0 +1,118 @@
+from functools import cached_property
+import pandas as pd
+import re
+
+
+class MplusModel:
+    CommondNames = ("TITLE", "DATA", "VARIABLE", "DEFINE",
+              "MONTECARLO", "MODELPOPULATION", "MODELMISSING", "ANALYSIS",
+              "MODEL", "MODELINDIRECT", "MODELCONSTRAINT", "MODELTEST", "MODELPRIORS",
+              "OUTPUT", "SAVEDATA", "PLOT")
+    CommondLabels = ("TITLE", "DATA", "VARIABLE", "DEFINE",
+              "MONTECARLO", "MODEL POPULATION", "MODEL MISSING", "ANALYSIS",
+              "MODEL", "MODEL INDIRECT", "MODEL CONSTRAINT", "MODEL TEST", "MODEL PRIORS",
+              "OUTPUT", "SAVEDATA", "PLOT")
+
+    def __init__(self,
+                TITLE = None,
+                DATA = None,
+                VARIABLE = None,
+                DEFINE = None,
+                MONTECARLO = None,
+                MODELPOPULATION = None,
+                MODELMISSING = None,
+                ANALYSIS = None,
+                MODEL = None,
+                MODELINDIRECT = None,
+                MODELCONSTRAINT = None,
+                MODELTEST = None,
+                MODELPRIORS = None,
+                OUTPUT = None,
+                SAVEDATA = None,
+                PLOT = None,
+                usevariables = [],
+                pdata: pd.DataFrame = None,
+                autov = True,
+                imputed = False,
+                quiet = True,
+                 ) -> None:
+        self.TITLE = TITLE
+        self._DATA = DATA
+        self.VARIABLE = VARIABLE
+        self.DEFINE = DEFINE
+        self.MONTECARLO = MONTECARLO
+        self.MODELPOPULATION = MODELPOPULATION
+        self.MODELMISSING = MODELMISSING
+        self.ANALYSIS = ANALYSIS
+        self.MODEL = MODEL
+        self.MODELINDIRECT = MODELINDIRECT
+        self.MODELCONSTRAINT = MODELCONSTRAINT
+        self.MODELTEST = MODELTEST
+        self.MODELPRIORS = MODELPRIORS
+        self.OUTPUT = OUTPUT
+        self.SAVEDATA = SAVEDATA
+        self.PLOT = PLOT
+        self.usevariables = usevariables
+        self.pdata = pdata
+        self.autov = autov
+        self.imputed = imputed
+        self.quiet = quiet
+
+    @cached_property
+    def syntax(self)->str:
+        codes = []
+        for name, label in zip(self.CommondNames, self.CommondLabels):
+            content = getattr(self, name)
+            if content:
+                code = f'{label}:\n    {content};'
+                codes.append(code)
+        return '\n'.join(codes)
+    
+    @cached_property
+    def data_file(self)->str:
+        return f'{self.TITLE}.dat'
+    
+    @cached_property
+    def input_file(self)->str:
+        return f'{self.TITLE}.inp'
+    
+    @cached_property
+    def outpu_file(self)->str:
+        return f'{self.TITLE}.out'
+    
+    @cached_property
+    def DATA(self):
+        if self._DATA:
+            return self._DATA
+        return f'FILE = "{self.data_file}"'
+    
+    def detect_usevariables(self)->list[str]:
+        ptn = re.compile("^(.*)( by | BY | By | on | ON | On )(.*)$")
+        vnames = []
+        for line in self.MODEL.replace('\n', '').split(';'):
+            mt = ptn.match(line)
+            print(line, ptn.match(line))
+            if mt:
+                op = mt.group(2)
+                vnames += line.replace(op, ' ').split(' ')
+        cleaned = []
+        for v in vnames:
+            if v: 
+                if v not in cleaned:
+                    if self.pdata is not None:
+                        if v in self.pdata.columns:
+                            cleaned.append(v)
+                    else:
+                        cleaned.append(v)
+        return cleaned
+
+        
+    def gen_data_file(self):
+        df = self.pdata
+        if self.usevariables:
+            cols = self.usevariables
+        else:
+            cols = self.detect_usevariables()
+        print('cols:', cols)
+        subdf = df[cols]
+        subdf.to_csv(self.data_file, index=None, header=False, sep=' ')
---
+++ test/test.mplus.py
@@ -0,0 +1,48 @@
+from mpluspy.mplus import MplusModel
+import pandas as pd
+import re
+
+def split(names: str):
+    match = re.match(r'(?P<prefix>[A-Za-z]+)(?P<start>\d+)-([A-Za-z]+)(?P<end>\d+)', names)  
+    if not match:  
+        raise ValueError(f"Invalid range string format: {names}")  
+      
+    prefix = match.group('prefix')  # 提取前缀  
+    start_num = int(match.group('start'))  # 提取起始数字  
+    end_num = int(match.group('end'))  # 提取结束数字  
+      
+    # 生成变量名列表  
+    variable_names = [f"{prefix}{num:0{len(str(end_num))}d}" for num in range(start_num, end_num + 1)]  
+    return variable_names  
+
+igroups = [
+    {'name': 'AT', 'items': split('AT1-AT5')},
+    {'name': 'PBC', 'items': split('PBC1-PBC4')},
+    {'name': 'BI', 'items': split('BI1-BI4')},
+    {'name': 'EC', 'items': split('EC1-EC7')},
+    {'name': 'SN', 'items': split('SN1-SN5')},
+    {'name': 'AB', 'items': split('AB1-AB5')},
+    {'name': 'CF', 'items': split('CF1-CF4')},
+    {'name': 'PPHL', 'items': split('PPHL1-PPHL4')},
+    {'name': 'DPT', 'items': split('DPT1-DPT4')},
+    {'name': 'JE', 'items': split('JE1-JE3')},
+]
+
+bystring = ''
+for ig in igroups:
+    bystring += f'{ig["name"]} by {" ".join(ig["items"])};\n'
+
+print(igroups)
+
+pdata = pd.read_excel(r'D:\dev\notebooks\jobs\医生知识共享患者健康素养和医患关系\data.xlsx')
+
+model = MplusModel(
+    TITLE='验证性因子分析',
+    MODEL=bystring,
+    OUTPUT='stdxy',
+    pdata=pdata,
+)
+
+print(model.detect_usevariables())
+print(model.syntax)
+model.gen_data_file()
